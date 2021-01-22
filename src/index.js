206593f47c741c822bdc4b91d4af03e8ef3732d8
SHerdenZ <zuhio76@gmail.com>
2021-01-22 09:06:22 +0400

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let num = n;
+    if (num<0) {
+      num=-num;
+    };
+    let strNum = num.toString();
+    let arrNum = strNum.split('');
+    let revArrNum = arrNum.reverse();
+    let revSrtNum = revArrNum.join('');
+    return +revSrtNum;
 }
